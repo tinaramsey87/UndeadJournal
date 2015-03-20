e0d908af0dd3963f4bf137b1ac8b04bda3e74a55
Tina Ramsey <tina.ramsey87@gmail.com>
2015-03-20 15:30:10 -0700

    finish CRUD/L functionality for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,42 @@
 class CommentsController < ApplicationController
-  def index
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments.all
-  end
-
   def new
     @post = Post.find(params[:post_id])
-    @comment = Comment.new
+    @comment = @post.comments.new
   end
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.new(comment_params)
       if @comment.save
         flash[:notice] = "Comment successfully added"
         redirect_to post_path(@post)
       else
         render :new
       end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+      if @comment.update(comment_params)
+        flash[:notice] = "Comment successfully updated"
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:alert] = "Comment permanently deleted"
+    redirect_to post_path(@post)
   end
 
   private
